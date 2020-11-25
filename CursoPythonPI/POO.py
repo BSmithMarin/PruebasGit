315907388b0a94ae7e0883f83c7e44a0928ec209
BSMP <bsmith.marin@alumno.ucjc.edu>
2020-11-25 21:05:27 +0100

    Apredidio concepto de encapsulamiento

--- CursoPythonPI/POO.py
+++ CursoPythonPI/POO.py
@@ -1,22 +1,38 @@
 class Coche():
-    anchoChasis=120
-    largoChasis=250
-    ruedas=4
-    enMarcha=False
 
-    def Arrancar(self):
+    def __init__(self):
+
+        self.anchoChasis=120
+        self.largoChasis=250
+        self.__ruedas=4 #Se encapsula la variabel, no es accesible desde fuera de la clase
+        self.enMarcha=False
+
+    def Arrancar(self,arrancamos):
         print("Arranco")
-        self.enMarcha=True
+        self.enMarcha=arrancamos
+
     
     def estado(self):
         if(self.enMarcha):
-            return "El coche esta en amrcha"
+            return "El coche esta en marcha"
         else:
 
             return " El coche esta parado"
 
 miCoche=Coche()
 
-#miCoche.Arrancar()
+miCoche.Arrancar(True)
 
 print(miCoche.estado())
+
+print("----------Aqui creamos el segundo objeto------------")
+
+miCoche2=Coche()
+
+print(miCoche2.estado())
+
+miCoche2.ruedas=5 #Esta variable no tiene nada que ver con las variabel __ruedas
+                  # que se encuentra en el constructur de la clase Coche()
+
+print("el coche 2 tiene ",miCoche2.ruedas, "Ruedas")
+print("el coche 2 tiene ",miCoche2)
