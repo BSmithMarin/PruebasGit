451c72b8a2d13bba57feb1a2c5f8c366f6ea6ae3
BSMP <bsmith.marin@alumno.ucjc.edu>
2020-11-25 19:20:45 +0100

    Termiando tema excepciones

--- CursoPythonPI/prueba_Excepciones3.py
+++ CursoPythonPI/prueba_Excepciones3.py
@@ -1,4 +1,10 @@
+import math
+
 def EvaluEdad(edad):
+
+    if edad<0:
+        raise ZeroDivisionError("No se permiten edades negativas")
+
     if edad<20:
         return "Eres demasiado joven"
     elif edad<40:
@@ -8,4 +14,24 @@ def EvaluEdad(edad):
     elif edad<100:
         return "Cuidate"
 
-print(EvaluEdad(19))
+print(EvaluEdad(20))
+
+def calculaRaiz(numero):
+
+    if numero<0:
+
+        raise ValueError("El numero no opuede ser negativo")
+
+    else:
+        return math.sqrt(numero)
+
+op1= int(input("Introduce un numero: "))
+
+try:
+
+    print(calculaRaiz(op1))
+except ValueError as ErrorDeNumeroNegativo:
+
+    print(ErrorDeNumeroNegativo)
+
+print("Programa termiando")
