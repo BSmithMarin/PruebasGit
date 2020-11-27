6d044db7ec1862a6248070d4354b184db8aaeeb0
BSMP <bsmith.marin@alumno.ucjc.edu>
2020-11-27 21:57:34 +0100

    Importacion de modulos

---
+++ CursoPythonPI/Modulos/UsoFunciones.py
@@ -0,0 +1,7 @@
+
+#Importa todos los metodos, variables y clases del archivo FuncionesMatematicas
+#Se le cambia el nombre para que sea mas facil e intuitivo al usarse
+
+import FuncionesMatematicas as math
+
+print(math.suma(5,7))
